dfca984c367a11e9e1241a610a565f2a437eb844
Guilherme Lumbreras <116804525+guillerplr@users.noreply.github.com>
2024-11-02 20:36:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_excel('frente.xlsx')
+df = pd.read_excel('arquivo.xlsx')
 
 df = df.drop_duplicates(subset=['ID da conta'], keep='first', ignore_index=True)
 
-df.to_excel('frente_sem_duplicatas.xlsx', index=False)
+df.to_excel('arquivo_sem_duplicatas.xlsx', index=False)
