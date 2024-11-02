a34ad8c03e3ab0dfa55b123aee0d6c60e6b21b83
Guilherme Lumbreras <116804525+guillerplr@users.noreply.github.com>
2024-11-02 20:36:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.read_excel('arquivo.xlsx')
 
-df = df.drop_duplicates(subset=['ID da conta'], keep='first', ignore_index=True)
+df = df.drop_duplicates(subset=['Coluna com duplicatas'], keep='first', ignore_index=True)
 
 df.to_excel('arquivo_sem_duplicatas.xlsx', index=False)
